7d0081749615f4479760edfb5d3f189764833541
unknown <Artsiom_Kaliaha@EPBYGROW0225.grodno.epam.com>
2015-03-17 17:57:16 +0300

    pull request effort

--- app/src/main/java/amtt/epam/com/amtt/MainActivity.java
+++ app/src/main/java/amtt/epam/com/amtt/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
