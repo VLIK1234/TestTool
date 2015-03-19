54a216888074095106dc1c677604d0c61e57e853
Ivan_Bakach <vlik1234@gmail.com>
2015-03-19 14:24:13 +0300

    make drag and drop for float button

--- app/src/main/java/amtt/epam/com/amtt/MainActivity.java
+++ app/src/main/java/amtt/epam/com/amtt/MainActivity.java
@@ -1,18 +1,101 @@
 package amtt.epam.com.amtt;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import amtt.epam.com.amtt.UI.FloatingActionButton;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
+
+    private String msg;
+    private android.widget.RelativeLayout relativeLayouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        relativeLayouts = (RelativeLayout) findViewById(R.id.relative_layout);
+
+        final FloatingActionButton fabButton = new FloatingActionButton.Builder(this)
+                .withDrawable(getResources().getDrawable(R.mipmap.ic_action_help))
+                .withButtonColor(Color.WHITE)
+                .withGravity(Gravity.BOTTOM | Gravity.RIGHT)
+                .withMargins(0, 0, 0, 0)
+                .create();
+        fabButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData.Item item = new ClipData.Item((CharSequence)"Tag");
+
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+                ClipData dragData = new ClipData("Tag",
+                        mimeTypes, item);
+
+                // Instantiates the drag shadow builder.
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(fabButton);
+
+                // Starts the drag
+                v.startDrag(dragData,  // the data to be dragged
+                        myShadow,  // the drag shadow builder
+                        null,      // no need to use local data
+                        0          // flags (not currently used, set to 0)
+                );
+                return true;
+            }
+        });
+
+        // Create and set the drag event listener for the View
+        relativeLayouts.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int x_cord;
+                int y_cord;
+
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                        break;
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        Log.d(msg, "ACTION_DROP event");
+                        y_cord = (int) event.getY();
+                        x_cord = (int) event.getX();
+                        fabButton.setX(x_cord-fabButton.getWidth()/2);
+                        fabButton.setY(y_cord-fabButton.getHeight()/2);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
 
 
     @Override
