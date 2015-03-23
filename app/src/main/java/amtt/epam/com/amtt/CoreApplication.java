f69b79c67f7cedc4e826ced6d4edc52cff476b81
Ivan_Bakach <vlik1234@gmail.com>
2015-03-23 12:45:44 +0300

    fix problem with code and button

--- app/src/main/java/amtt/epam/com/amtt/CoreApplication.java
+++ app/src/main/java/amtt/epam/com/amtt/CoreApplication.java
@@ -1,23 +1,6 @@
 package amtt.epam.com.amtt;
 
 import android.app.Application;
-import android.content.ClipData;
-import android.content.ClipDescription;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PixelFormat;
-import android.util.Log;
-import android.view.DragEvent;
-import android.view.Gravity;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import amtt.epam.com.amtt.service.HUD;
 
 /**
  * Created by Ivan_Bakach on 19.03.2015.
@@ -27,6 +10,6 @@ public class CoreApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        startService( new Intent(this, HUD.class));
+
     }
 }
--- app/src/main/java/amtt/epam/com/amtt/MainActivity.java
+++ app/src/main/java/amtt/epam/com/amtt/MainActivity.java
@@ -2,7 +2,6 @@ package amtt.epam.com.amtt;
 
 import android.content.ClipData;
 import android.content.ClipDescription;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
@@ -13,118 +12,33 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RelativeLayout;
 
-import amtt.epam.com.amtt.ui.FloatingActionButton;
-
+import amtt.epam.com.amtt.service.TopButtonService;
 
 public class MainActivity extends ActionBarActivity{
 
     private String msg;
-    private android.widget.RelativeLayout relativeLayouts;
+    private View relativeLayouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Context context = getApplicationContext();
-
-        relativeLayouts = (RelativeLayout) findViewById(R.id.relative_layout);
-
-        final FloatingActionButton fabButton = new FloatingActionButton.Builder(this)
-                .withDrawable(getResources().getDrawable(R.mipmap.ic_action_help))
-                .withButtonColor(Color.WHITE)
-                .withGravity(Gravity.BOTTOM | Gravity.RIGHT)
-                .withMargins(0, 0, 0, 0)
-                .create();
-        fabButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                ClipData.Item item = new ClipData.Item((CharSequence)"Tag");
-
-                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-                ClipData dragData = new ClipData("Tag",
-                        mimeTypes, item);
-
-                // Instantiates the drag shadow builder.
-                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(fabButton);
-
-                // Starts the drag
-                v.startDrag(dragData,  // the data to be dragged
-                        myShadow,  // the drag shadow builder
-                        null,      // no need to use local data
-                        0          // flags (not currently used, set to 0)
-                );
-                return true;
-            }
-        });
-
-        fabButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Create and set the drag event listener for the View
-        relativeLayouts.setOnDragListener(new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-                int x_cord;
-                int y_cord;
-
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
-                        break;
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
-                        break;
-                    case DragEvent.ACTION_DRAG_EXITED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
-                        break;
-                    case DragEvent.ACTION_DRAG_LOCATION:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        break;
-                    case DragEvent.ACTION_DRAG_ENDED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
-                        break;
-                    case DragEvent.ACTION_DROP:
-                        Log.d(msg, "ACTION_DROP event");
-                        y_cord = (int) event.getY();
-                        x_cord = (int) event.getX();
-                        fabButton.setX(x_cord - fabButton.getWidth() / 2);
-                        fabButton.setY(y_cord - fabButton.getHeight() / 2);
-                        break;
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
+        startService( new Intent(this, TopButtonService.class));
     }
 
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
