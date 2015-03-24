d4db2848585235fa90a0ea2dd9e044da2f4c5617
unknown <Artsiom_Kaliaha@EPBYGROW0225.grodno.epam.com>
2015-03-24 18:53:47 +0300

    initial changes

---
+++ app/src/main/java/amtt/epam/com/amtt/api/ApiJira.java
@@ -0,0 +1,10 @@
+package amtt.epam.com.amtt.api;
+
+/**
+ * Created by Artsiom_Kaliaha on 24.03.2015.
+ */
+public class ApiJira {
+
+    private static final String BASE_PATH = "https://orionjira.atlassian.net/rest/api/latest/";
+
+}
