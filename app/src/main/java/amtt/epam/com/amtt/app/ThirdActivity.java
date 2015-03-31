c9eed749300d97b636e5302ba7e2b0947477c4ee
Ivan_Bakach <vlik1234@gmail.com>
2015-03-31 12:59:38 +0300

    example with programmatically create button

--- app/src/main/java/amtt/epam/com/amtt/app/ThirdActivity.java
+++ app/src/main/java/amtt/epam/com/amtt/app/ThirdActivity.java
@@ -1,10 +1,10 @@
 package amtt.epam.com.amtt.app;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import amtt.epam.com.amtt.R;
 
@@ -13,15 +13,34 @@ import amtt.epam.com.amtt.R;
  */
 public class ThirdActivity extends BaseActivity {
 
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.custom_view_layout);
+        linearLayout = (LinearLayout) findViewById(R.id.custom_linear);
+        int[] buttonDrawableResourceIds = new int[] {
+                R.drawable.button_add, R.drawable.button_share, R.drawable.button_auth,
+                R.drawable.button_screen,R.drawable.button_bug_rep
+        };
+        Button[] arrayButton = new Button[5];
+        int counter = 0;
+        for (Button item : arrayButton) {
+            item = new Button(this);
+            item.setText(String.valueOf(counter));
+            item.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            linearLayout.addView(item);
+            counter++;
+        }
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setContentView(R.layout.activity_third);
+//        setContentView(R.layout.activity_third);
     }
 }
