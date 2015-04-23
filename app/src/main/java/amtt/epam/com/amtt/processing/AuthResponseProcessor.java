b1968f86a9c91f555f01b45dc9472a9ae7730fad
IstiN (Uladzimir Klyshevich) <istin2007@gmail.com>
2015-04-23 17:07:44 +0300

    jira authorization improvements

--- app/src/main/java/amtt/epam/com/amtt/processing/AuthResponseProcessor.java
+++ app/src/main/java/amtt/epam/com/amtt/processing/AuthResponseProcessor.java
@@ -1,17 +1,27 @@
 package amtt.epam.com.amtt.processing;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.client.HttpClient;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import amtt.epam.com.amtt.BuildConfig;
 import amtt.epam.com.amtt.bo.auth.AuthResponse;
 import amtt.epam.com.amtt.bo.auth.AuthResponseDeserializer;
+import amtt.epam.com.amtt.util.IOUtils;
+import io.fabric.sdk.android.services.network.HttpRequest;
 
 /**
  * Created by Artsiom_Kaliaha on 10.04.2015.
@@ -20,17 +30,28 @@ public class AuthResponseProcessor extends ResponseProcessor {
 
     @Override
     public String process(HttpResponse httpResponse) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
-        StringBuilder builder = new StringBuilder();
-        for (String line; (line = reader.readLine()) != null;) {
-            builder.append(line).append("\n");
+        //TODO
+        int statusCode = httpResponse.getStatusLine().getStatusCode();
+        if (statusCode == HttpStatus.SC_NOT_FOUND) {
+            //TODO use correct type of message
+            throw new AuthenticationException("not found");
+        } else if (statusCode == HttpStatus.SC_UNAUTHORIZED) {
+            throw new AuthenticationException();
         }
 
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(AuthResponse.class, new AuthResponseDeserializer())
-                .create();
-        AuthResponse authResponse = gson.fromJson(builder.toString(), AuthResponse.class);
+        InputStream content = null;
+        InputStreamReader inputStreamReader = null;
+        try {
+            content = httpResponse.getEntity().getContent();
+            inputStreamReader = new InputStreamReader(content, "UTF-8");
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(AuthResponse.class, new AuthResponseDeserializer())
+                    .create();
+            AuthResponse authResponse = gson.fromJson(inputStreamReader, AuthResponse.class);
 
-        return authResponse.getPrettyResponse();
+            return authResponse.getPrettyResponse();
+        } finally {
+            IOUtils.close(content, inputStreamReader);
+        }
     }
 }
