6a900cba329771922273a9c647b9562a4ae6de32
Iryna Monchanka <iryna_monchanka@epam.com>
2015-06-04 09:47:18 +0300

    Overengineering

--- app/src/main/java/amtt/epam/com/amtt/util/UIUtil.java
+++ app/src/main/java/amtt/epam/com/amtt/util/UIUtil.java
@@ -1,39 +1,34 @@
 package amtt.epam.com.amtt.util;
 
-import android.content.Context;
 import android.content.res.Configuration;
 import android.util.DisplayMetrics;
 
 /**
- * Created by Artsiom_Kaliaha on 27.05.2015.
+ @author Artsiom_Kaliaha
+ @version on 27.05.2015
  */
+
 public final class UIUtil {
 
-    private static final Context sContext;
-
-    static {
-        sContext = ContextHolder.getContext();
-    }
-
     public static boolean isLandscape() {
-        return sContext.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        return ContextHolder.getContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     public static DisplayMetrics getDisplayMetrics() {
-        return sContext.getResources().getDisplayMetrics();
+        return ContextHolder.getContext().getResources().getDisplayMetrics();
     }
 
     public static int getStatusBarHeight() {
         int result = 0;
-        int resourceId = sContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int resourceId = ContextHolder.getContext().getResources().getIdentifier("status_bar_height", "dimen", "android");
         if (resourceId > 0) {
-            result = sContext.getResources().getDimensionPixelSize(resourceId);
+            result = ContextHolder.getContext().getResources().getDimensionPixelSize(resourceId);
         }
         return result;
     }
 
     public static int getOrientation() {
-        return sContext.getResources().getConfiguration().orientation;
+        return ContextHolder.getContext().getResources().getConfiguration().orientation;
     }
 
 }
