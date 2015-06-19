5d8c231cbf9028263c4a95d93eea6bbd520aa731
Artsiom Kaliaha <Artsiom_Kaliaha@epam.com>
2015-06-19 11:56:05 +0300

    TextInput inherits from TextInputLayout

--- app/src/main/java/amtt/epam/com/amtt/view/TextInput.java
+++ app/src/main/java/amtt/epam/com/amtt/view/TextInput.java
@@ -1,99 +1,53 @@
 package amtt.epam.com.amtt.view;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Rect;
 import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 
 import com.android.internal.util.Predicate;
 
 import java.util.Map;
 
-import amtt.epam.com.amtt.R;
-import amtt.epam.com.amtt.util.Constants;
-import amtt.epam.com.amtt.util.InputsUtil;
-
 /**
  * Created by Artsiom_Kaliaha on 12.06.2015.
  */
-public class TextInput extends FrameLayout {
+public class TextInput extends TextInputLayout {
 
-    private EditText mEditText;
-    private TextInputLayout mTextInputLayout;
+    private EditText mET;
 
     private int mInitialTextLength;
     private String mDefaultErrorText;
     private boolean isErrorTookPlace;
 
-    private Map<Predicate<EditText>,CharSequence> mValidationMap;
+    private Map<Predicate<EditText>, CharSequence> mValidationMap;
 
     public TextInput(Context context) {
-        super(context);
-        initContent(context, null, 0, 0);
+        this(context, null);
     }
 
     public TextInput(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
+        initContent(context, attrs);
     }
 
-    public TextInput(Context context, AttributeSet attrs, int defStyleAttr) {
-        this(context, attrs, defStyleAttr, 0);
+    private void initContent(Context context, AttributeSet attrs) {
+        mET = new EditText(context, attrs);
+        addView(mET, new ViewGroup.LayoutParams(context, attrs));
     }
 
-    public TextInput(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr);
-        initContent(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-    private void initContent(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.TextInput, defStyleAttr, defStyleRes);
-
-        this.setFocusableInTouchMode(true);
-        this.setFocusable(true);
-
-        mEditText = new EditText(context, attrs);
-        mEditText.setOnFocusChangeListener(new OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (isErrorTookPlace && !hasFocus && isEditTextEmpty()) {
-                    mTextInputLayout.setError(mDefaultErrorText);
-                } else if (isErrorTookPlace) {
-                    mTextInputLayout.setError(Constants.Symbols.EMPTY);
-                }
-            }
-        });
-
-        try {
-            mDefaultErrorText = getContext().getString(R.string.enter_prefix) + typedArray.getString(R.styleable.TextInput_defaultErrorText);
-        } finally {
-            typedArray.recycle();
-        }
-
-        mTextInputLayout = new TextInputLayout(context);
-        mTextInputLayout.addView(mEditText, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        mTextInputLayout.setErrorEnabled(true);
-        addView(mTextInputLayout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-    }
-
-    public void setValidationMap(Map<Predicate<EditText>,CharSequence> validationMap) {
+    public void setValidationMap(Map<Predicate<EditText>, CharSequence> validationMap) {
         mValidationMap = validationMap;
     }
 
     public void validate() {
         isErrorTookPlace = false;
-        for (Map.Entry<Predicate<EditText>,CharSequence> pair : mValidationMap.entrySet()) {
-            if (pair.getKey().apply(mEditText)) {
-                mTextInputLayout.setError(pair.getValue());
+        for (Map.Entry<Predicate<EditText>, CharSequence> pair : mValidationMap.entrySet()) {
+            if (pair.getKey().apply(mET)) {
+                //mTextInputLayout.setError(pair.getValue());
                 isErrorTookPlace = true;
                 break;
             }
@@ -101,24 +55,19 @@ public class TextInput extends FrameLayout {
     }
 
     public Editable getText() {
-        return mEditText.getText();
+        return mET.getText();
     }
 
     public void setText(CharSequence text) {
-        mEditText.setText(text);
+        mET.setText(text);
     }
 
     private boolean isEditTextEmpty() {
         if (mInitialTextLength != 0) {
-            return mEditText.getText().length() <= mInitialTextLength;
+            return mET.getText().length() <= mInitialTextLength;
         } else {
-            return TextUtils.isEmpty(mEditText.getText());
+            return TextUtils.isEmpty(mET.getText());
         }
     }
 
-    @Override
-    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
-        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
-        mEditText.requestFocus();
-    }
 }
