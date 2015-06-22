c6724c20dc7847c9347120e4dd4d7d3c5bdc0c24
Ivan_Bakach <vlik1234@gmail.com>
2015-06-22 13:26:54 +0300

    fix null filename

--- app/src/main/java/amtt/epam/com/amtt/util/FileUtil.java
+++ app/src/main/java/amtt/epam/com/amtt/util/FileUtil.java
@@ -1,20 +1,16 @@
 package amtt.epam.com.amtt.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import android.support.annotation.NonNull;
+
+import java.io.File;
 
 /**
  * Created by Ivan_Bakach on 10.06.2015.
  */
 public class FileUtil {
 
-    public static String getFileName(String filePath){
-        Pattern p = Pattern.compile("[-_0-9a-zA-Z]*[.]\\w{0,5}");
-        Matcher m = p.matcher(filePath);
-        if (m.find()) {
-            return m.group();
-        }else{
-            return "";
-        }
+    public static String getFileName(@NonNull String filePath){
+        int slash = filePath.lastIndexOf(File.pathSeparatorChar);
+        return filePath.substring(slash);
     }
 }
