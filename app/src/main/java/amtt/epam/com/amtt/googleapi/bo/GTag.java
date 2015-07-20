f247bc0a6bfa964dd9edc98eb5d1585f56d71ef0
Irina.Monchenko <monchenko.i.d@gmail.com>
2015-07-20 18:07:31 +0300

    Added tags table

---
+++ app/src/main/java/amtt/epam/com/amtt/googleapi/bo/GTag.java
@@ -0,0 +1,70 @@
+package amtt.epam.com.amtt.googleapi.bo;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import amtt.epam.com.amtt.database.object.DatabaseEntity;
+import amtt.epam.com.amtt.googleapi.database.contentprovider.GSUri;
+import amtt.epam.com.amtt.googleapi.database.table.TagsTable;
+
+/**
+ * @author Iryna Monchenko
+ * @version on 20.07.2015
+ */
+public class GTag extends DatabaseEntity<GTag>{
+
+    private int mId;
+    private String mIdLinkTestCase;
+    private String mName;
+
+    public GTag() {
+    }
+
+    public GTag(Cursor cursor) {
+        if (cursor.getPosition() == -1) {
+            cursor.moveToNext();
+        }
+        mId = cursor.getInt(cursor.getColumnIndex(TagsTable._ID));
+        mIdLinkTestCase = cursor.getString(cursor.getColumnIndex(TagsTable._TESTCASE_ID_LINK));
+        mName = cursor.getString(cursor.getColumnIndex(TagsTable._NAME));
+    }
+
+    @Override
+    public GTag parse(Cursor cursor) {
+        return null;
+    }
+
+    public GTag(int id, String idLinkTestCase, String name) {
+        this.mId = id;
+        this.mIdLinkTestCase = idLinkTestCase;
+        this.mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    @Override
+    public int getId() {
+        return mId;
+    }
+
+    @Override
+    public Uri getUri() {
+        return GSUri.TESTCASE.get();
+    }
+
+    @Override
+    public ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(TagsTable._TESTCASE_ID_LINK, mIdLinkTestCase);
+        values.put(TagsTable._NAME, mName);
+        return values;
+    }
+}
+
