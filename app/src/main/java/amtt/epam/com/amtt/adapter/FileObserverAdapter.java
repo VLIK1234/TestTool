5928e3666b97bebee5162254cffd57d71b2330c9
Ivan_Bakach <vlik1234@gmail.com>
2015-09-22 23:54:48 +0300

    add on click listener and open folder by click

--- app/src/main/java/amtt/epam/com/amtt/adapter/FileObserverAdapter.java
+++ app/src/main/java/amtt/epam/com/amtt/adapter/FileObserverAdapter.java
@@ -1,11 +1,11 @@
 package amtt.epam.com.amtt.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.File;
@@ -19,10 +19,16 @@ import amtt.epam.com.amtt.R;
  */
 public class FileObserverAdapter extends RecyclerView.Adapter<FileObserverAdapter.ViewHolder>{
 
-    private File[] mListFile;
+    public interface IClickListener {
+        void onItemClick(File chooseFile);
+    }
 
-    public FileObserverAdapter(File[] listFile) {
+    private File[] mListFile;
+    private IClickListener mListener;
+
+    public FileObserverAdapter(File[] listFile, IClickListener listener) {
         mListFile = listFile;
+        mListener = listener;
     }
 
     @Override
@@ -33,13 +39,19 @@ public class FileObserverAdapter extends RecyclerView.Adapter<FileObserverAdapte
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         holder.fileName.setText(mListFile[position].getName());
         if (mListFile[position].isDirectory()) {
             holder.fileIcon.setImageDrawable(CoreApplication.getContext().getResources().getDrawable(R.drawable.ic_file_folder));
         } else {
             holder.fileIcon.setImageDrawable(CoreApplication.getContext().getResources().getDrawable(R.drawable.ic_file));
         }
+        holder.rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onItemClick(mListFile[position]);
+            }
+        });
     }
 
     @Override
@@ -49,11 +61,13 @@ public class FileObserverAdapter extends RecyclerView.Adapter<FileObserverAdapte
 
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final LinearLayout rootView;
         public final TextView fileName;
         public final ImageView fileIcon;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            rootView = (LinearLayout) itemView.findViewById(R.id.ll_root_view);
             fileName = (TextView) itemView.findViewById(R.id.tv_file_name);
             fileIcon = (ImageView) itemView.findViewById(R.id.iv_file_icon);
         }
--- app/src/main/java/amtt/epam/com/amtt/ui/fragments/BrowserFragment.java
+++ app/src/main/java/amtt/epam/com/amtt/ui/fragments/BrowserFragment.java
@@ -20,24 +20,25 @@ import amtt.epam.com.amtt.adapter.FileObserverAdapter;
  * @author IvanBakach
  * @version on 22.09.2015
  */
-public class BrowserFragment extends Fragment {
+public class BrowserFragment extends Fragment implements FileObserverAdapter.IClickListener{
 
     private static final int SPAN_COUNT = 3;
     private FileObserverAdapter mFileObserverAdapter;
+    private RecyclerView mRecyclerView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_browser, container, false);
-        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.rv_file_browser);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), SPAN_COUNT));
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.rv_file_browser);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), SPAN_COUNT));
         File currentDirectory = Environment.getExternalStorageDirectory();
         for (File file: currentDirectory.listFiles()) {
             Log.d("List", file.getName());
         }
 
-        mFileObserverAdapter = new FileObserverAdapter(currentDirectory.listFiles());
-        recyclerView.setAdapter(mFileObserverAdapter);
+        mFileObserverAdapter = new FileObserverAdapter(currentDirectory.listFiles(), this);
+        mRecyclerView.setAdapter(mFileObserverAdapter);
         return view;
     }
     @Override
@@ -48,5 +49,14 @@ public class BrowserFragment extends Fragment {
     public static BrowserFragment newInstance(){
         BrowserFragment browserFragment = new BrowserFragment();
         return browserFragment;
+    }
+
+    @Override
+    public void onItemClick(File chooseFile) {
+        if (chooseFile.isDirectory()) {
+            mFileObserverAdapter = new FileObserverAdapter(chooseFile.listFiles(), this);
+            mRecyclerView.setAdapter(mFileObserverAdapter);
+        }
+
     }
 }
