ab6aa36f5f3eb85c52f123090ebb51ce73e2b516
Ivan_Bakach <vlik1234@gmail.com>
2015-09-24 08:59:32 +0300

    remove redundant logic

--- app/src/main/java/amtt/epam/com/amtt/ui/activities/ShareFileActivity.java
+++ app/src/main/java/amtt/epam/com/amtt/ui/activities/ShareFileActivity.java
@@ -1,5 +1,6 @@
 package amtt.epam.com.amtt.ui.activities;
 
+import android.os.Environment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -29,33 +30,15 @@ public class ShareFileActivity extends BaseActivity implements BrowserFilesFragm
         mPager = (ViewPager) findViewById(R.id.vp_folders_layout);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
-        if (savedInstanceState == null) {
-            addBrowserFilesFragment(FileUtil.getUsersCacheDir());
-        }
+//        addBrowserFilesFragment(FileUtil.getUsersCacheDir());
+        addBrowserFilesFragment(Environment.getExternalStorageDirectory().getPath());
     }
 
     private void addBrowserFilesFragment(String folderPath) {
         BrowserFilesFragment fragment = BrowserFilesFragment.newInstance(folderPath, this);
-        int indexExistFragment = searchExistFragment(fragment);
         mFilesFragments.add(fragment);
-        mPagerAdapter.getItem(mFilesFragments.size() - 1);
         mPagerAdapter.notifyDataSetChanged();
-//        if (searchExistFragment(fragment) == -1) {
-//        } else {
-//            mFilesFragments.add(indexExistFragment, fragment);
-//            mFilesFragments.remove(indexExistFragment + 1);
-//            mPagerAdapter.getItem(indexExistFragment);
-//            mPagerAdapter.notifyDataSetChanged();
-//        }
-    }
-
-    public int searchExistFragment(BrowserFilesFragment filesFragment){
-        for (int i = 0; i < mFilesFragments.size();i++) {
-            if (mFilesFragments.get(i).getFolderPath().equals(filesFragment.getFolderPath())) {
-                return i;
-            }
-        }
-        return -1;
+        mPager.setCurrentItem(mPager.getChildCount()-1, true);
     }
 
     @Override
