a6d2d516adc9e1c73b17358e38ca1ae97c6f1982
Ivan_Bakach <vlik1234@gmail.com>
2015-09-28 15:43:43 +0300

    change sharing files to everywhere

--- app/src/main/java/amtt/epam/com/amtt/helper/SharingToEmailHelper.java
+++ app/src/main/java/amtt/epam/com/amtt/helper/SharingToEmailHelper.java
@@ -32,11 +32,8 @@ public class SharingToEmailHelper {
     public static void senAttachmentFile(Activity activity, String message, String filePath){
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setType("*/*");
-        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{getCurrentUsersEmail(activity)});
-        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Attachment from amtt");
-        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, message);
         emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(filePath)));
-        activity.startActivity(Intent.createChooser(emailIntent, "Send e-mail..."));
+        activity.startActivity(Intent.createChooser(emailIntent, "Send to..."));
     }
 
     private static String getCurrentUsersEmail(Context context){
