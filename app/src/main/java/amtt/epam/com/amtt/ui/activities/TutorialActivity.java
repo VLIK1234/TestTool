14d913783c2355b9c87ba4e2bf3b26fd275ce59a
Ivan_Bakach <vlik1234@gmail.com>
2015-10-15 16:34:01 +0300

    add TutorialActivity and library ViewPagerIndicator

--- app/src/main/java/amtt/epam/com/amtt/ui/activities/TutorialActivity.java
+++ app/src/main/java/amtt/epam/com/amtt/ui/activities/TutorialActivity.java
@@ -1,44 +1,67 @@
 package amtt.epam.com.amtt.ui.activities;
 
-import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.viewpagerindicator.CirclePageIndicator;
 
 import amtt.epam.com.amtt.R;
-import amtt.epam.com.amtt.ui.fragments.BrowserFilesFragment;
+import amtt.epam.com.amtt.ui.fragments.TutorialFragment;
 
 /**
  * @author IvanBakach
  * @version on 14.10.2015
  */
-public class TutorialActivity extends Activity {
+public class TutorialActivity extends AppCompatActivity {
+
+    private int[] mDrawablesArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
-        int[] drawablesArray = {R.drawable.ic_action_action_list};
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        mDrawablesArray = new int[]{R.drawable.background_auth_method, R.drawable.ic_action_delete, R.drawable.ic_create_ticket};
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_folders_layout);
+        TutorialPagerAdapter pagerAdapter = new TutorialPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        CirclePageIndicator titleIndicator = (CirclePageIndicator) findViewById(R.id.pi_view_pager_indicator);
+        titleIndicator.setViewPager(viewPager);
+        Button closeButton = (Button) findViewById(R.id.bt_got_it);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
-//    private class TutorialPagerAdapter extends FragmentStatePagerAdapter {
-//        public TutorialPagerAdapter(FragmentManager fm) {
-//            super(fm);
-//        }
-//
-//        @Override
-//        public int getItemPosition(Object object) {
-//            return POSITION_NONE;
-//        }
-//
-//        @Override
-//        public BrowserFilesFragment getItem(int position) {
-//            return BrowserFilesFragment.newInstance(mFolderPaths.get(position), mSharedFilePaths, ShareFilesActivity.this);
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return mFolderPaths.size();
-//        }
-//    }
+    private class TutorialPagerAdapter extends FragmentStatePagerAdapter {
+        public TutorialPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+
+        @Override
+        public TutorialFragment getItem(int position) {
+            return TutorialFragment.newInstance(mDrawablesArray[position]);
+        }
+
+        @Override
+        public int getCount() {
+            return mDrawablesArray.length;
+        }
+    }
 }
--- app/src/main/java/amtt/epam/com/amtt/ui/fragments/TutorialFragment.java
+++ app/src/main/java/amtt/epam/com/amtt/ui/fragments/TutorialFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 import amtt.epam.com.amtt.R;
 
@@ -15,16 +18,32 @@ import amtt.epam.com.amtt.R;
  */
 public class TutorialFragment extends Fragment {
 
+    public static final String KEY_DRAWABLE_ID = "drawableId";
+    private ImageView mTutorialScreen;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_browser, container, false);
+        View view = inflater.inflate(R.layout.fragment_tutorial, container, false);
+        mTutorialScreen = (ImageView) view.findViewById(R.id.iv_tutorial_screen);
         return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mTutorialScreen.setImageResource(getDrawableId());
+    }
 
+    public static TutorialFragment newInstance(int drawableId){
+        TutorialFragment tutorialFragment = new TutorialFragment();
+        Bundle args = new Bundle();
+        args.putInt(KEY_DRAWABLE_ID, drawableId);
+        tutorialFragment.setArguments(args);
+        return tutorialFragment;
+    }
+
+    private int getDrawableId() {
+        return getArguments().getInt(KEY_DRAWABLE_ID);
     }
 }
